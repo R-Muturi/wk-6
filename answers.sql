f4319901225c29016ee074add6437d35674b624e
R.Muturi <rachaelwangui005@gmail.com>
2025-09-25 20:07:40 +0300

    Update answers.sql

--- answers.sql
+++ answers.sql
@@ -6,3 +6,22 @@ SELECT
 FROM
  employees AS E
  INNER JOIN offices AS O ON E.officeCode = O.officeCode;
+
+SELECT
+ P.productName,
+ P.productVendor,
+ P.productLine
+FROM
+ products AS P
+ LEFT JOIN productlines AS PL ON P.productLine = PL.productLine;
+
+SELECT
+ O.orderDate,
+ O.shippedDate,
+ O.status,
+ O.customerNumber
+FROM
+ customers AS C
+ RIGHT JOIN orders AS O ON C.customerNumber = O.customerNumber
+LIMIT
+ 10;
